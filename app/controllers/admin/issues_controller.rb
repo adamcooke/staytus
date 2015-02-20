280ee2aa3aa9ed5582d473fb0efa128835dfef16
Adam Cooke <adam@atechmedia.com>
2015-02-20 18:08:08 +0000

    editting & deleting issues

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -23,10 +23,23 @@ class Admin::IssuesController < Admin::BaseController
     @issue = Issue.new(safe_params)
     @issue.user = current_user
     if @issue.save
-      redirect_to admin_issues_path, :notice => 'Added succesfully'
+      redirect_to admin_issue_path(@issue), :notice => 'Issue has been added successfully.'
     else
       render 'new'
     end
+  end
+
+  def update
+    if @issue.update(safe_params)
+      redirect_to admin_issue_path(@issue), :notice => 'Issue has been updated successfully.'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @issue.destroy
+    redirect_to admin_issues_path, :notice => 'Issue has been removed successfully.'
   end
 
   private
