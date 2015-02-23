902e1d1eb255a2bff392a401254a1b0da5b2cc62
Adam Cooke <adam@atechmedia.com>
2015-02-23 18:09:39 +0000

    tidy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,61 @@
 Rails.application.routes.draw do
 
   namespace :admin do
+    #
+    # Authentication
+    #
     get 'login' => 'sessions#new'
     post 'login' => 'sessions#create'
     delete 'logout' => 'sessions#destroy'
 
+    # Settings
+    #
     get 'settings' => 'settings#edit'
     patch 'settings' => 'settings#update'
     get 'settings/design' => 'settings#design'
-
-    get 'infrastructure' => 'infrastructure#index'
-
     resources :users
     resources :services
     resources :service_statuses
+
+    #
+    # Issues
+    #
     resources :issues do
       get 'resolved', :on => :collection
       resources :issue_updates
     end
+
+    #
+    # Maintenances
+    #
     resources :maintenances do
       get 'completed', :on => :collection
       post 'toggle', :on => :member
       resources :maintenance_updates
     end
+
+    #
+    # Misc. Admin Routes
+    #
     get 'helpers/:action', :controller => 'helpers'
+
+    #
+    # Admin Root
+    #
     root 'dashboard#index'
   end
 
+  #
+  # Setup Wizard
+  #
+  match 'setup/:action', :controller => 'setup', :as => 'setup', :via => [:get, :post]
+
+  #
+  # Public Site Paths
+  #
   get 'issue/:id' => 'pages#issue'
   get 'maintenance/:id' => 'pages#maintenance'
   get 'history' => 'pages#history'
-
-  match 'setup/:action', :controller => 'setup', :as => 'setup', :via => [:get, :post]
-
   get 'robots.txt' => 'pages#robots'
   root 'pages#index'
 
