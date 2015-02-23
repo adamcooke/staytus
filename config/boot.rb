67a0cb5449aaafc654b88d2447a6ff81ddb9ad35
Adam Cooke <adam@atechmedia.com>
2015-02-23 18:41:00 +0000

    set the default port to 8787 and listen on all interfaces

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,17 @@
 ENV['RAILS_ENV'] ||= 'production'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup'
+
+# Set the default port for the application
+require 'rails/commands/server'
+module Rails
+  class Server
+    alias :original_default_options :default_options
+    def default_options
+      original_default_options.merge(
+        :Port => 8787,
+        :Host => '0.0.0.0'
+      )
+    end
+  end
+end
