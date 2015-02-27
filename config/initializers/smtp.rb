6370b9dbcc2ca11bd15f0b850a37f576704184e2
Adam Cooke <adam@atechmedia.com>
2015-02-27 19:11:59 +0000

    add SMTP configuration

---
+++ config/initializers/smtp.rb
@@ -0,0 +1,11 @@
+if ENV['STAYTUS_SMTP_HOSTNAME']
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address    => ENV['STAYTUS_SMTP_HOSTNAME'],
+    :user_name  => ENV['STAYTUS_SMTP_USERNAME'],
+    :password   => ENV['STAYTUS_SMTP_PASSWORD']
+  }
+else
+  puts "\e[33m=> You haven't configured an SMTP server. Mail will be sent using sendmail.\e[0m"
+  ActionMailer::Base.delivery_method = :sendmail
+end
