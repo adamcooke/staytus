2d67d73b9bfa6f6ed1b7c388075568d4d2182247
glaszig <glaszig@gmail.com>
2015-08-17 22:00:36 +0200

    adding api endpoint to create issues

--- api/controllers/issues_api_controller.rb
+++ api/controllers/issues_api_controller.rb
@@ -7,6 +7,33 @@ controller :issues do
     end
   end
 
+  action :create do
+    param :title, :required => true, :type => String
+    param :initial_update, :required => false, :type => String
+    param :state, :required => false, :type => String
+    param :services, :required => true, :type => Array
+    param :status, :required => true, :type => String
+    param :notify, :required => false
+
+    action do
+      issue_params = {
+        title: params.title,
+        initial_update: params.initial_update,
+        state: params.state,
+        service_ids: Service.where(permalink: params.services).pluck(:id),
+        service_status_id: ServiceStatus.where(permalink: params.status).first.id,
+        notify: params.notify
+      }
+
+      issue = Issue.new issue_params
+      if issue.save
+        structure issue, :full => true
+      else
+        error :validation_error, issue.errors
+      end
+    end
+  end
+
   action :info do
     param :issue, :required => true, :type => Integer
     action do
