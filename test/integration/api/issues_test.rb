c34f7254bf09c7e97dc30dab6b16d31a5c826e8f
glaszig <glaszig@gmail.com>
2015-08-17 22:00:36 +0200

    adding api endpoint to create issues

--- test/integration/api/issues_test.rb
+++ test/integration/api/issues_test.rb
@@ -12,4 +12,18 @@ class IssuesTest < ActionDispatch::IntegrationTest
 
     assert_equal 'issue 1', json_response[:data][:title]
   end
+
+  test "creates issues" do
+    post_json "/api/v1/issues/create", {
+      title: 'new issue',
+      initial_update: 'houston!!!',
+      services: [ 'api' ],
+      state: 'investigating',
+      status: 'partial-outage'
+    }
+
+    assert_equal 'success', json_response[:status]
+    assert_equal 'new issue', json_response[:data][:title]
+    assert_equal 'investigating', json_response[:data][:state]
+  end
 end
