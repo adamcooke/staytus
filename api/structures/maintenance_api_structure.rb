750815eede16bf855a3861cdb235bb7af93a80db
Coen Spoor <nachtwerk_vfx@me.com>
2017-11-07 14:52:11 +0100

    added maintenance api structure

---
+++ api/structures/maintenance_api_structure.rb
@@ -0,0 +1,15 @@
+structure :maintenance do
+  basic :id
+  basic :title
+  full :description
+  full :start_at
+  full :finish_at
+  full :length_in_minutes
+  full :created_at
+  full :updated_at
+
+  expansion :services
+
+  expansion :updates
+
+end
