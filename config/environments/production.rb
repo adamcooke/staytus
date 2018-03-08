4dce989d54697708ba6debf4bfd00f168f9ae4e9
Adam Cooke <me@adamcooke.io>
2018-03-08 11:50:44 +0000

    force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = ENV['FORCE_SSL'].to_i == 1
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
