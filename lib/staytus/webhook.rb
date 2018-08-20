082224d22a231ae7c03e403ba689e70b6bccb309
Sebastian Plattner <plattner@puzzle.ch>
2018-08-20 13:04:33 +0200

    Fix output

--- lib/staytus/webhook.rb
+++ lib/staytus/webhook.rb
@@ -7,8 +7,8 @@ module Staytus
       # Send an email to the given recipient
       #
       def call(attributes = {})
-        logger.info "Webhook"
-        logger.info attributes.issue.to_json
+        puts "Webhook"
+        puts attributes.issue.to_json
 
       end
 
