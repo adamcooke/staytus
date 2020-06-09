ac6808453f6d0053401f4efb1caf0ebebb316aa0
Daniel Rice <daniel.rice@ldstudios.co>
2020-06-09 12:26:00 -0500

    Configure SMTP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :letter_opener #TODO - does this work on linux???
+  config.action_mailer.default_url_options = { :host => "localhost", port: (ENV['PORT'] || 3000) }
+  config.action_mailer.asset_host = "http://localhost:#{ENV.fetch('PORT', 3000)}"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,26 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_url_options = {
+    protocol: 'https',
+    host: ENV.fetch('ACTION_MAILER_HOST') { "#{ENV['HEROKU_APP_NAME']}.herokuapp.com" }
+  }
+  config.action_mailer.asset_host = "https://" + ENV.fetch('ACTION_MAILER_HOST') { "#{ENV['HEROKU_APP_NAME']}.herokuapp.com" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
