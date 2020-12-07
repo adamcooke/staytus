2286189a1113a9b20f5426650771f6de5a2cd693
naft-a <googs.bs@gmail.com>
2020-12-07 12:12:23 +0000

    chore: removes links, paragraphs and images processing

--- lib/staytus/markdown_renderer.rb
+++ lib/staytus/markdown_renderer.rb
@@ -21,35 +21,5 @@ module Staytus
       code
     end
 
-    def image(link, title, alt_text)
-      # Get classes out of the ALT text
-      alt_text, classes, title = (alt_text || '').split('*').map(&:strip)
-
-      # Return our image tag
-      img = "<img src='#{link}' alt='#{alt_text}' title='#{title}' class='#{classes}' />"
-      if title
-        "#{img}<span class='imageCaption #{classes}'>#{title}</span>"
-      else
-        img
-      end
-    end
-
-    def link(link, title, content)
-      if link =~ /\A\//
-        link = "#{Home.web_host_with_protocol}#{link}"
-      end
-      "<a href='#{link}' title='#{title}'>#{content}</a>"
-    end
-
-    def paragraph(text)
-      class_name = nil
-      text.gsub!(/\A@([a-z\- ]+):\s*/i) do
-        class_name = Regexp.last_match(1)
-        ''
-      end
-      text.gsub!(/\R+/, '<br />') if @options[:hard_wrap]
-      "<p class='#{class_name}'>#{text}</p>"
-    end
-
   end
 end
